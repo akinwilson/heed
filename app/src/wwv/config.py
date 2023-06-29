300ce1afcbbe6393dd522884dac698c380d4b8a5
akinwilson <akinola.antony.wilson@gmail.com>
2023-06-29 18:01:59 +0100

    hard coding output directories. need to dynamically choose model output locations

--- app/src/wwv/config.py
+++ app/src/wwv/config.py
@@ -1,220 +1,231 @@
-'''
+"""
 Processing, training, data, model and augmentation configuration 
 definition
 
 Have created model dataclasses to tried to isolated factors of concerns on per model basis 
 Note, there is overlap in attributes at the moment amongst the data classes. 
-'''
+"""
 import os
-import numpy as np 
-from pathlib import Path 
-from typing import Tuple,List
+import numpy as np
+from pathlib import Path
+from typing import Tuple, List
 from dataclasses import dataclass, field
 
 
-@dataclass 
+@dataclass
 class Fitting:
-    ''' Parameters fitting of model. Corresponds all models, with focus on HST and custom learning scheduler'''
-    batch_size: int  = 16
-    learning_rate: float = 1e-3 
-    max_epoch : int = 50
-    num_workers : int  = 18
-    lr_scheduler_epoch : List  =  field(default_factory=  lambda : [10,20,30])
-    lr_rate : List = field(default_factory = lambda :  [0.02, 0.05, 0.1])
+    """Parameters fitting of model. Corresponds all models, with focus on HST and custom learning scheduler"""
+
+    batch_size: int = 16
+    learning_rate: float = 1e-3
+    max_epoch: int = 50
+    num_workers: int = 18
+    lr_scheduler_epoch: List = field(default_factory=lambda: [10, 20, 30])
+    lr_rate: List = field(default_factory=lambda: [0.02, 0.05, 0.1])
     es_patience: int = 10
-    fast_dev_run: bool =  field(default=False)
-    resume_from_checkpoint : str = field(default=None)
-    train_bs : int = 32
-    val_bs : int  = 32
-    test_bs : int = 32
+    fast_dev_run: bool = field(default=False)
+    resume_from_checkpoint: str = field(default=None)
+    train_bs: int = 32
+    val_bs: int = 32
+    test_bs: int = 32
 
 
 @dataclass
 class CNNAE:
-    '''CNN autoencoder architectural parameters'''
-    audio_feature: str = "pmc" 
+    """CNN autoencoder architectural parameters"""
+
+    audio_feature: str = "pmc"
     model_name: str = "CNNAE"
-    model_dir: str =  "/home/akinwilson/Code/pytorch/output/model"
-    max_sample_len : int = 32000 
-    onnx_op_set : int =  12
+    model_dir: str = "/home/useraye/Code/pytorch/output/model"
+    max_sample_len: int = 32000
+    onnx_op_set: int = 12
 
 
 @dataclass
 class CVCNNAE:
-    '''Conditional variational convolutional neural network autoencoder'''
-    audio_feature: str = "pmc" 
+    """Conditional variational convolutional neural network autoencoder"""
+
+    audio_feature: str = "pmc"
     model_name: str = "CVCNNAE"
-    model_dir: str =  "/home/akinwilson/Code/pytorch/output/model"
-    max_sample_len : int = 32000 
-    onnx_op_set : int =  12
-
-
+    model_dir: str = "/home/useraye/Code/pytorch/output/model"
+    max_sample_len: int = 32000
+    onnx_op_set: int = 12
 
 
 @dataclass
 class SSCNNAE:
-    '''Semi supervised autoencoding classifier hybrid'''
-    audio_feature: str = "pmc" 
-    model_name: str = "SSCNNAE"
-    model_dir: str =  "/home/akinwilson/Code/pytorch/output/model"
-    max_sample_len : int = 32000 
-    onnx_op_set : int =  12
+    """Semi supervised autoencoding classifier hybrid"""
 
+    audio_feature: str = "pmc"
+    model_name: str = "SSCNNAE"
+    model_dir: str = "/home/useraye/Code/pytorch/output/model"
+    max_sample_len: int = 32000
+    onnx_op_set: int = 12
 
 
 @dataclass
 class AEClassifier:
-    '''Dense example classifier'''
-    model_name: str = "AE_Classifier"
-    model_dir: str =  "/home/akinwilson/Code/pytorch/output/model"
-    max_sample_len : int = 32000 
-    onnx_op_set : int =  12
-    audio_feature : str = "pcm"
+    """Dense example classifier"""
 
+    model_name: str = "AE_Classifier"
+    model_dir: str = "/home/useraye/Code/pytorch/output/model"
+    max_sample_len: int = 32000
+    onnx_op_set: int = 12
+    audio_feature: str = "pcm"
 
 
 @dataclass
 class ResNet:
-    '''Residual network architectural parameters'''
-    num_blocks: List = field( default_factory = lambda : [4, 6, 18, 3] )
-    audio_feature: str = "mfcc" 
-    model_name: str = "ResNet"
-    model_dir: str =  "/home/akinwilson/Code/pytorch/output/model"
-    max_sample_len : int = 32000 
-    onnx_op_set : int =  12
+    """Residual network architectural parameters"""
 
-@dataclass 
+    num_blocks: List = field(default_factory=lambda: [4, 6, 18, 3])
+    audio_feature: str = "mfcc"
+    model_name: str = "ResNet"
+    model_dir: str = "/home/useraye/Code/pytorch/output/model"
+    max_sample_len: int = 32000
+    onnx_op_set: int = 12
+
+
+@dataclass
 class HTSwin:
-    '''for htSwin hyperparamater'''
+    """for htSwin hyperparamater"""
+
     window_size: int = 8
-    spec_size: int =  256
-    patch_size: int = 4 
+    spec_size: int = 256
+    patch_size: int = 4
     stride: Tuple[int] = (2, 2)
-    num_head: List = field(default_factory= lambda : [4,8,16,8])
+    num_head: List = field(default_factory=lambda: [4, 8, 16, 8])
     dim: int = 48
     num_classes: int = 1
-    depth: List = field(default_factory= lambda : [2,6,4,2])
-    audio_feature: str = "pcm" # required for dataloading pipeline to extract correct features 
-    model_name: str = "HSTAT" # 
-    max_sample_len : int = 32000 
-    onnx_op_set : int =  17
-    model_dir: str =  "/home/akinwilson/Code/pytorch/output/model"
+    depth: List = field(default_factory=lambda: [2, 6, 4, 2])
+    audio_feature: str = (
+        "pcm"  # required for dataloading pipeline to extract correct features
+    )
+    model_name: str = "HSTAT"  #
+    max_sample_len: int = 32000
+    onnx_op_set: int = 17
+    model_dir: str = "/home/useraye/Code/pytorch/output/model"
 
 
 @dataclass
 class DeepSpeech:
-    '''DeepSpeech network architectural parameters'''
-    audio_feature: str = "mfcc" 
+    """DeepSpeech network architectural parameters"""
+
+    audio_feature: str = "mfcc"
     model_name: str = "DeepSpeech"
-    model_dir: str =  "/home/akinwilson/Code/pytorch/output/model"
-    max_sample_len : int = 32000
-    onnx_op_set : int =  12
+    model_dir: str = "/home/useraye/Code/pytorch/output/model"
+    max_sample_len: int = 32000
+    onnx_op_set: int = 12
 
 
 @dataclass
 class LeeNet:
-    '''LeeNet network architectural parameters'''
-    audio_feature: str = "mfcc" 
+    """LeeNet network architectural parameters"""
+
+    audio_feature: str = "mfcc"
     model_name: str = "LeeNet"
-    model_dir: str =  "/home/akinwilson/Code/pytorch/output/model"
-    max_sample_len : int = 32000 
-    onnx_op_set : int =  12
+    model_dir: str = "/home/useraye/Code/pytorch/output/model"
+    max_sample_len: int = 32000
+    onnx_op_set: int = 12
+
 
 @dataclass
 class MobileNet:
-    '''MobileNet network architectural parameters'''
-    audio_feature: str = "mfcc" 
+    """MobileNet network architectural parameters"""
+
+    audio_feature: str = "mfcc"
     model_name: str = "MobileNet"
-    model_dir: str =  "/home/akinwilson/Code/pytorch/output/model"
-    max_sample_len : int = 32000 
-    onnx_op_set : int =  12
-
-
+    model_dir: str = "/home/useraye/Code/pytorch/output/model"
+    max_sample_len: int = 32000
+    onnx_op_set: int = 12
 
 
 @dataclass
 class Signal:
-    ''' Parameters for signal processing of model. Corresponds ot HTS atm but want generic signal param class'''
-    sample_rate : int = 16000
-    audio_duration : int = 2
-    clip_samples : int = int(sample_rate * audio_duration)
-    window_size : int = 512
-    hop_size  : int= 256
-    mel_bins : int = 64
-    fmin : int = 50
-    fmax : int = 14000
-    shift_max : int = int(clip_samples * 0.5)
-    enable_tscam : bool = field(default=True, metadata= { "help":  "Enbale the token-semantic layer"})
-    
+    """Parameters for signal processing of model. Corresponds ot HTS atm but want generic signal param class"""
 
-
+    sample_rate: int = 16000
+    audio_duration: int = 2
+    clip_samples: int = int(sample_rate * audio_duration)
+    window_size: int = 512
+    hop_size: int = 256
+    mel_bins: int = 64
+    fmin: int = 50
+    fmax: int = 14000
+    shift_max: int = int(clip_samples * 0.5)
+    enable_tscam: bool = field(
+        default=True, metadata={"help": "Enbale the token-semantic layer"}
+    )
 
 
 @dataclass
 class Feature:
-    ''' Parameters for signal processing of model. Corresponds to signal proccessing for edge device detector'''
-    audio_duration: float = 1.5
-    sample_rate:int = 16000
-    window_len:int = int( 0.040 * sample_rate )
-    window_step:int = int( 0.020 *sample_rate ) 
-    mel_coefficients:int = 16
-    mel_filters:int = 40
-    num_fft :int = 1024
-    low_freq: float = 20.
-    high_freq: float = 8000.
-    melspec_kwargs: dict = field ( init=False,metadata={"help": "Parameters for mel spectrograme extraction of MFCC generation"}) 
-                
-    def __post_init__(self):
-        self.melspec_kwargs =  {"n_fft": self.num_fft,
-                                 "win_length": self.window_len,
-                                 "hop_length": self.window_step,
-                                 "n_mels": self.mel_filters,
-                                 "onesided":True,
-                                 "center": True,
-                                 "pad_mode":"reflect",
-                                 "f_min" : self.low_freq,
-                                 "f_max" : self.high_freq} 
+    """Parameters for signal processing of model. Corresponds to signal proccessing for edge device detector"""
 
+    audio_duration: float = 1.5
+    sample_rate: int = 16000
+    window_len: int = int(0.040 * sample_rate)
+    window_step: int = int(0.020 * sample_rate)
+    mel_coefficients: int = 16
+    mel_filters: int = 40
+    num_fft: int = 1024
+    low_freq: float = 20.0
+    high_freq: float = 8000.0
+    melspec_kwargs: dict = field(
+        init=False,
+        metadata={
+            "help": "Parameters for mel spectrograme extraction of MFCC generation"
+        },
+    )
+
+    def __post_init__(self):
+        self.melspec_kwargs = {
+            "n_fft": self.num_fft,
+            "win_length": self.window_len,
+            "hop_length": self.window_step,
+            "n_mels": self.mel_filters,
+            "onesided": True,
+            "center": True,
+            "pad_mode": "reflect",
+            "f_min": self.low_freq,
+            "f_max": self.high_freq,
+        }
 
 
 @dataclass
 class DataPath:
-    ''' Input data paths, checks for files that are supposed to exist during init to. Like train.csv etc.'''
-    root_data_dir : str  = field(
+    """Input data paths, checks for files that are supposed to exist during init to. Like train.csv etc."""
+
+    root_data_dir: str = field(
         metadata={
             "help": "Root directory of training, validation and testing csv files"
         }
     )
 
-    model_name : str = field(
-        metadata={
-            "help": "Name of model. Will be used to create model_dir"
-        }
+    model_name: str = field(
+        metadata={"help": "Name of model. Will be used to create model_dir"}
     )
-    root_model_dir : str  = field(
-        metadata={
-            "help": "Root directory for outputs of training process"
-        }
+    root_model_dir: str = field(
+        metadata={"help": "Root directory for outputs of training process"}
     )
 
-    model_dir : str = field(
+    model_dir: str = field(
         init=False,
         metadata={
             "help": "Directory for outputs of training process. Will be created after init"
-        }
+        },
     )
 
-    
     def __post_init__(self):
         expected_files = ["train.csv", "test.csv", "val.csv"]
         if not all(x in os.listdir(self.root_data_dir) for x in expected_files):
-            raise FileNotFoundError(f"Expected: {expected_files}\nTo exist in dir: {self.root_data_dir}.\nOnly found: {os.listdir(self.root_data_dir)}")
+            raise FileNotFoundError(
+                f"Expected: {expected_files}\nTo exist in dir: {self.root_data_dir}.\nOnly found: {os.listdir(self.root_data_dir)}"
+            )
 
-        model_dir = (Path(self.root_model_dir) / self.model_name)
+        model_dir = Path(self.root_model_dir) / self.model_name
         model_dir.mkdir(parents=True, exist_ok=True)
         self.model_dir = str(model_dir)
-        
-
 
 
 # class Config:
@@ -231,13 +242,13 @@ class DataPath:
 #         self.path = params['path']
 #         self.verbose = params['verbose']# True
 #         self.lr_scheduler_epoch = [100,200,300]
-#         self.lr_rates = [0.02, 0.05, 0.1] 
+#         self.lr_rates = [0.02, 0.05, 0.1]
 
 #     @property
 #     def max_sample_len(self):
 #         return int(self.audio_duration * self.sr)
 
-#     @property 
+#     @property
 #     def processing_output_shape(self):
 #         attrname = 'audio_feature_param'
 #         if self.audio_feature == "mfcc":
@@ -255,14 +266,12 @@ class DataPath:
 #             return self.max_sample_len
 
 
-
-# 75 *  0.02 / 16000 = a 
-
+# 75 *  0.02 / 16000 = a
 
 
-# MODEL_DIR = "/home/akinwilson/Code/pytorch/output/model"  # location of root outputs
-# DATA_DIR = "/home/akinwilson/Code/pytorch/dataset/keywords" # root location of meta training data csv files 
-# LR_RANGE = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5][1] 
+# MODEL_DIR = "/home/useraye/Code/pytorch/output/model"  # location of root outputs
+# DATA_DIR = "/home/useraye/Code/pytorch/dataset/keywords" # root location of meta training data csv files
+# LR_RANGE = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5][1]
 # BATCH_SIZE_RANGE = [1,2,16, 32, 64, 128, 256][2]
 # EPOCH_RANGE = [1, 10, 30, 50, 100, 1000][1]
 # ES_PATIENCE_RANGE = [1, 10, 20, 100, 200][2]
@@ -281,8 +290,8 @@ class DataPath:
 #         "data_dir": DATA_DIR,
 #         "pretrained_name_or_path": PRETRAINED_MODEL_NAME_OR_PATH
 #         },
-#     "fit_param": {"init_lr":LR_RANGE, "weight_decay":0.0001, "max_epochs":EPOCH_RANGE, "gamma": 0.1,"es_patience":ES_PATIENCE_RANGE}, 
-#     "data_param":{"train_batch_size": BATCH_SIZE_RANGE, "val_batch_size": BATCH_SIZE_RANGE,"test_batch_size": BATCH_SIZE_RANGE}, 
+#     "fit_param": {"init_lr":LR_RANGE, "weight_decay":0.0001, "max_epochs":EPOCH_RANGE, "gamma": 0.1,"es_patience":ES_PATIENCE_RANGE},
+#     "data_param":{"train_batch_size": BATCH_SIZE_RANGE, "val_batch_size": BATCH_SIZE_RANGE,"test_batch_size": BATCH_SIZE_RANGE},
 #     "audio_feature": AUDIO_FEATURE_OPT,
 #     "audio_feature_param": { "mfcc":{"sr":16000,"n_mfcc":20,"norm": 'ortho',"verbose":True,"ref":1.0,"amin":1e-10,"top_db":80.0,"hop_length":512,},
 #                             "spectrogram":{"sr":16000, "n_fft":2048, "win_length":None,"n_mels":128,"hop_length":512,"window":'hann',"center":True,"pad_mode":'reflect',"power":2.0,"htk":False,"fmin":0.0,"fmax":None,"norm":1,"trainable_mel":False,"trainable_STFT":False,"verbose": True },
