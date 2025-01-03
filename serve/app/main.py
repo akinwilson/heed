923d8624fbc5f82b6f535204701ec212e33a08f4
ola <battlestation.home>
2025-01-03 10:20:12 +0000

    tied serving

--- serve/app/main.py
+++ serve/app/main.py
@@ -2,31 +2,34 @@ import base64
 import os
 import sys
 import uvicorn
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, Request  # , status
 from fastapi.logger import logger
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import RedirectResponse, JSONResponse
+
+# from fastapi.encoders import jsonable_encoder
+# from fastapi.responses import RedirectResponse, JSONResponse
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
+
+# from fastapi.staticfiles import StaticFiles
+
 # import base64
 import torch
-import onnxruntime 
+import onnxruntime
 from config import CONFIG
-import numpy as np 
-import time 
-import torchaudio 
+import numpy as np
+import time
+import torchaudio
 from exception_handler import validation_exception_handler, python_exception_handler
 from schema import *
 
 # Initialize API Server
 app = FastAPI(
-    title="Wakeword Detection Verificator",
-    description="Wakeword verification model for Sky Glass",
+    title="Audio Key-word Spotting",
+    description="Torch-based deep learning model trained to 'spot' a key-word; hello glsss",
     version="0.0.1",
     terms_of_service=None,
     contact=None,
-    license_info=None
+    license_info=None,
 )
 
 # Allow CORS for local debugging
@@ -45,31 +48,29 @@ async def startup_event():
     logger.info(f"Running envirnoment: {CONFIG['ENV']}")
     logger.info(f"PyTorch using device: {CONFIG['DEVICE']}")
 
-    providers = [os.environ.get("EXECUTION_PROVIDER"),]
-    # providers = ["CPUExecutionProvider",]
-
-    inference_session = onnxruntime.InferenceSession(CONFIG['ONNX_MODEL_PATH'],
-                                                     providers=providers)
+    providers = [
+        os.environ.get("EXECUTION_PROVIDER"),
+    ]
+    inference_session = onnxruntime.InferenceSession(
+        CONFIG["ONNX_MODEL_PATH"], providers=providers
+    )
 
     # add model and other preprocess tools too app state
-    app.package = {
-        "torch_model": None,
-        "onnx_session": inference_session
-    }
+    app.package = {"torch_model": None, "onnx_session": inference_session}
 
 
-@app.post('/api/v1/predict',
-          response_model=InferenceResult,
-          responses={422: {"model": ErrorResponse},
-                     500: {"model": ErrorResponse}}
-          )
-def do_predict(request: Request, body: InferenceInput):
+@app.post(
+    "/api/v1/predict",
+    response_model=InferenceResult,
+    responses={422: {"model": ErrorResponse}, 500: {"model": ErrorResponse}},
+)
+def predict(request: Request, body: InferenceInput):
     """
     Perform prediction on input data
     """
 
-    logger.info('API predict called')
-    logger.info(f'input: {body}')
+    logger.info("API predict called")
+    logger.info(f"predict() rquestion body input: {body}")
     MAX_INPUT_DIM = 48000
     ################################################################
     #                   Preprocess before onnx
@@ -79,50 +80,50 @@ def do_predict(request: Request, body: InferenceInput):
     decoded_wav = base64.decodebytes(body.content)
     x = np.frombuffer(decoded_wav, dtype=np.int16)
     # (sample_len,)
-    x = x[np.newaxis, ...]    
+    x = x[np.newaxis, ...]
     # (channel_dim, sample_len)
-    pad_up = MAX_INPUT_DIM - x.shape[-1] 
+    pad_up = MAX_INPUT_DIM - x.shape[-1]
     if pad_up < 0:
         # Truncating case
-        X = x[...,: MAX_INPUT_DIM]
+        X = x[..., :MAX_INPUT_DIM]
     else:
-        # Padding case 
-        X = np.pad(x, ((0,0), (0, pad_up)), 'constant')
+        # Padding case
+        X = np.pad(x, ((0, 0), (0, pad_up)), "constant")
 
     X = X[np.newaxis, ...]
-    # (batch_dim, channel_dim, sample_len)    
-    X  = X / np.iinfo(np.int16).max # normalise to range -1 to 1
-    
-    kwargs = {
-        "window_fn": torch.hann_window,
-        "wkwargs":
-        { 
-            "device": "cpu"
-            }   
-        }
+    # (batch_dim, channel_dim, sample_len)
+    X = X / np.iinfo(np.int16).max  # normalise to range -1 to 1
+    ###############################################################
+    # we could prefer this preprocessing to be part of the model
+    # deployment. This is why MLFlow introducted the idea of
+    # a model signature;
+    ###############################################################
+    kwargs = {"window_fn": torch.hann_window, "wkwargs": {"device": "cpu"}}
     x_mfcc_trans = torchaudio.transforms.MFCC(melkwargs=kwargs)
     x_mfcc = x_mfcc_trans(torch.tensor(X).float()).numpy()
     ################################################################
     ################################################################
     # Inference with Onnx runtime
-    result = app.package["onnx_session"].run(None, {"input_mfcc":x_mfcc.astype(np.float32)})
+    result = app.package["onnx_session"].run(
+        None, {"input_mfcc": x_mfcc.astype(np.float32)}
+    )
     f = time.time()
     # print("result", result)
     logger.info(f"Results from onnx: {result}")
     # Need to implement predictor method. logits currently being returned.
-    ww_prob = result[0][0][0]
+    kw_prob = result[0][0][0]
     # prepare json for returning
     return {
-            "wake_word_probability": ww_prob , 
-            "prediction": 1.0 if ww_prob > CONFIG['DECISION_THRESHOLD'] else 0.0, 
-            "false_alarm_probability": 1-ww_prob,
-            "decision_threshold": CONFIG['DECISION_THRESHOLD'],
-            "wwvm_version" : CONFIG['MODEL_VERSION'],
-            "inference_time" : f-s 
-            }
+        "keyword_probability": kw_prob,
+        "prediction": 1.0 if kw_prob > CONFIG["DECISION_THRESHOLD"] else 0.0,
+        "false_alarm_probability": 1 - kw_prob,
+        "decision_threshold": CONFIG["DECISION_THRESHOLD"],
+        "kwsm_version": CONFIG["MODEL_VERSION"],
+        "inference_time": f - s,
+    }
 
 
-@app.get('/about')
+@app.get("/about")
 def show_about():
     """
     Get deployment information, for debugging
@@ -139,13 +140,12 @@ def show_about():
         "torch.version.cuda": torch.version.cuda,
         "torch.backends.cudnn.version()": torch.backends.cudnn.version(),
         "torch.backends.cudnn.enabled": torch.backends.cudnn.enabled,
-        "nvidia-smi": bash('nvidia-smi')
+        "nvidia-smi": bash("nvidia-smi"),
     }
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # server api
-    uvicorn.run("main:app", host="0.0.0.0", port=8080,
-                reload=True, debug=True)
-                # , log_config="log.ini"
-                # )
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, debug=True)
+    # , log_config="log.ini"
+    # )
--- serve/app/schema.py
+++ serve/app/schema.py
@@ -8,43 +8,48 @@ from pydantic import BaseModel, Field
 with open("base64audio_label_1", "rb") as file:
     BASE64 = file.read()
 
+
 class InferenceInput(BaseModel):
     """
     Input values for model inference
     """
-    content: Optional[bytes] = Field(..., example=BASE64, title='Base64 encoded PCM waveform')
 
-
+    content: Optional[bytes] = Field(
+        ..., example=BASE64, title="Base64 encoded PCM waveform"
+    )
 
 
 class InferenceResult(BaseModel):
     """
-    Inference result from the model: As previously defied 
+    Inference result from the model: As previously defied
     """
-    wake_word_probability: float = Field(..., example=0.987526, title='Probablity of wakeword')
-    prediction: float = Field(..., example=1.0, title='Predicted label')
-    false_alarm_probability: float = Field(... , example=0.001, title="False alarm probability")
-    decision_threshold: float =  Field(... , example=0.55, title="Decision threshold")
-    wwvm_version : str =   Field(... , example="model-v0.0.1", title="Model version")
-    inference_time : float =  Field(... , example=0.2560, title="Inference time") 
-        
 
+    wake_word_probability: float = Field(
+        ..., example=0.987526, title="Probablity of keyword"
+    )
+    prediction: float = Field(..., example=1.0, title="Predicted label")
+    false_alarm_probability: float = Field(
+        ..., example=0.001, title="False alarm probability"
+    )
+    decision_threshold: float = Field(..., example=0.55, title="Decision threshold")
+    wwvm_version: str = Field(..., example="model-v0.0.1", title="Model version")
+    inference_time: float = Field(..., example=0.2560, title="Inference time")
 
 
 class InferenceResponse(BaseModel):
     """
     Output response for model inference
     """
-    error: bool = Field(..., example=False, title='Whether there is error')
+
+    error: bool = Field(..., example=False, title="Whether there is error")
     result: InferenceResult = ...
-
-
 
 
 class ErrorResponse(BaseModel):
     """
     Error response for the API
     """
-    error: bool = Field(..., example=True, title='Whether there is error')
-    message: str = Field(..., example='', title='Error message')
-    traceback: str = Field(None, example='', title='Detailed traceback of the error')
+
+    error: bool = Field(..., example=True, title="Whether there is error")
+    message: str = Field(..., example="", title="Error message")
+    traceback: str = Field(None, example="", title="Detailed traceback of the error")
