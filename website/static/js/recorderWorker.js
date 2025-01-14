727581644682cfaafbace0cb5709902199755fce
nameduser aconite <nameduser@aconite.com>
2025-01-14 12:49:51 +0000

    static javascript file from stackoverflow

--- website/static/js/recorderWorker.js
+++ website/static/js/recorderWorker.js
@@ -1,35 +1,37 @@
-(function(window){
+(function (window) {
 
-    var WORKER_PATH = 'recorderWorker.js';
-    var Recorder = function(source, cfg){
-      var config = cfg || {};
-      var bufferLen = config.bufferLen || 4096;
-      this.context = source.context;
-      this.node = this.context.createScriptProcessor(bufferLen, 2, 2);
-      var worker = new Worker(config.workerPath || WORKER_PATH);
-      worker.postMessage({
-        command: 'init',
-        config: {
-          sampleRate: this.context.sampleRate
-        }
-      });
-      var recording = false,
-        currCallback;
-
-      this.node.onaudioprocess = function(e){
-        if (!recording) return;
-        worker.postMessage({
-          command: 'record',
-          buffer: [
-            e.inputBuffer.getChannelData(0),
-            e.inputBuffer.getChannelData(1)
-          ]
-        });
+  var WORKER_PATH = 'recorderWorker.js';
+  var Recorder = function (source, cfg) {
+    var config = cfg || {};
+    var bufferLen = config.bufferLen || 4096;
+    this.context = source.context;
+    this.node = this.context.createScriptProcessor(bufferLen, 2, 2);
+    var worker = new Worker(config.workerPath || WORKER_PATH);
+    worker.postMessage({
+      command: 'init',
+      config: {
+        sampleRate: this.context.sampleRate
       }
+    });
+    var recording = false,
+      currCallback;
 
-      this.configure = function(cfg){
-        for (var prop in cfg){
-          if (cfg.hasOwnProperty(prop)){
-            config[prop] = cfg[prop];
-          }
+    this.node.onaudioprocess = function (e) {
+      if (!recording) return;
+      worker.postMessage({
+        command: 'record',
+        buffer: [
+          e.inputBuffer.getChannelData(0),
+          e.inputBuffer.getChannelData(1)
+        ]
+      });
+    }
+
+    this.configure = function (cfg) {
+      for (var prop in cfg) {
+        if (cfg.hasOwnProperty(prop)) {
+          config[prop] = cfg[prop];
         }
+      }
+    }
+  })
