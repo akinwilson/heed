40738a232d6cd49fdedc9ed0c89a6a2d5091582d
nameduser aconite <nameduser@aconite.com>
2025-01-18 18:03:56 +0000

    correctly decoding ogg file to raw pcm bytes, averaging across channels to plot waveform

--- website/heed/views.py
+++ website/heed/views.py
@@ -10,6 +10,10 @@ import base64
 import urllib
 import random
 import time
+import numpy as np 
+from pydub import AudioSegment
+from io import BytesIO
+
 
 plt_colours = ["b", "g", "r", "c", "m", "y"]
 
@@ -26,11 +30,12 @@ def plot_bar(prob=0.75):
     uri = urllib.parse.quote(plot_encoded)
     return uri 
 
-def plot(audio_bytes=None, c=plt_colours[1]):
+def plot(pcm=None, c=plt_colours[1]):
     fig, ax = plt.subplots()
-    x = np.linspace(0, 2 * np.pi, 1000)
-    y = [np.cos(_x) for _x in x]
-    ax.plot(x, y, "*", color=c)
+    # x = np.linspace(0, 2 * np.pi, 1000)
+    # y = [np.cos(_x) for _x in x]
+    # ax.plot(x, y, "*", color=c)
+    ax.plot(pcm,color=c)
     buf = io.BytesIO()
     fig.savefig(buf, format="png")
     buf.seek(0)
@@ -53,15 +58,35 @@ def upload(request):
     if request.method == "POST":
         if request.FILES.get("audio", False):
             file = request.FILES['audio']
-            print(f"file:{file}")
-            print("file.file", file.file)
-            import numpy as np 
-            audio_samples = np.frombuffer(file.file, dtype=np.int16)
+            # print(f"file:{file}")
+            # print("file.file", file.file)
+            # import numpy as np 
+            # print("dir(file.file)", dir(file.file))
+            audio_bytes = BytesIO(b"".join(file.file.readlines()))
+            # print("audio_file: ", audio_file)
+            print("audio_file bytes: ", audio_bytes)
 
-            print("file.__dict__",file.__dict__ )
 
-    
-    return JsonResponse({'image_uri':plot_bar()}) 
+            audio = AudioSegment.from_ogg(audio_bytes) 
+            audio.set_frame_rate(16000)
+
+            # audio_file = audiosegment.from_file(audio_file, format='ogg')
+
+            print("audio: ", audio)
+            print("dir(audio): ", dir(audio))
+            print("channel: ", audio.channels)
+
+            pcm = np.array(audio.get_array_of_samples(), dtype=np.float32).reshape((-1, audio.channels)) / (1 << (8 * audio.sample_width - 1))
+
+            # print("array: ", audio.get_array_of_samples())
+            # pcm = np.array(audio.get_array_of_samples())
+            print("pcm.shape: ", pcm.shape)
+            pcm_avg = np.mean(pcm, axis=1)
+            # audio_samples = np.frombuffer(file.file, dtype=np.int16)
+            print("pcm_avg.shape: ", pcm_avg.shape)
+            # print("file.__dict__",file.__dict__ )
+
+    return JsonResponse({'image_uri':plot(pcm=pcm_avg)}) 
     
 def index(request):
     print("hello")
